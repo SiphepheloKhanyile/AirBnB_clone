a382733d30fb9300452addeba1340fd408041f60
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-01-14 01:46:18 +0200

    Adding task

--- models/base_model.py
+++ models/base_model.py
@@ -13,17 +13,26 @@ class BaseModel:
     """
     BaseModel class
     """
-    def __init__(self) -> None:
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs): # pylint: disable=unused-argument
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key in ("created_at", "updated_at"):
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
         string representation of Object when object is printed
         "[<class name>] (<self.id>) <self.__dict__>"
         """
-        return f"[{type(self).__name__}] ({self.id}) {self.__dict__})"
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """
@@ -47,7 +56,3 @@ class BaseModel:
         inst_dict["created_at"] = self.created_at.isoformat()
         inst_dict["updated_at"] = self.updated_at.isoformat()
         return inst_dict
-
-
-if __name__ == "__main__":
-    pass
